d7163dfbf629ab56f6481cdb10ea1ef94d414626
Филатов Алексей Алексеевич <Filatov.A.Alekseev@sberbank.ru>
2021-10-25 16:29:55 +0300

    removed 2 hermione tests

--- test/hermione/test.hermione.js
+++ test/hermione/test.hermione.js
@@ -44,15 +44,6 @@ describe('проверка статических страниц', async functio
         });
     });
 
-    it('адаптивность страницы доставки', async function () {
-        await this.browser.setWindowSize(320, 896);
-        const browser = this.browser;
-        await this.browser.url('/hw/store/delivery');
-        await browser.assertView('plain', '.Application', {
-            allowViewportOverflow: true,
-        });
-    });
-
     it('навигационное меню должно скрываться за "гамбургер"', async function () {
         await this.browser.setWindowSize(320, 896);
         const navigationToggler = await this.browser.$('.navbar-toggler');
