3b79fb4640d0626a2e589d04ab225e0acfba7699
Lily <lily.lilps@yandex.ru>
2021-10-29 17:51:26 +0300

    added test page 1

---
+++ src/client/pages/Test1.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import { cn } from '@bem-react/classname';
+
+const bem = cn('Test1');
+
+export const Test1: React.FC = () => {
+    return (
+        <div className={bem()}>
+            <Helmet title="Test1" />
+                <h1>Test1</h1>
+                <p>
+                    No description, website, or topics provided. 
+                </p>
+        </div>
+    );
+}
